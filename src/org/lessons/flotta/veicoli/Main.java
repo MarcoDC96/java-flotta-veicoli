5028f01706466684cd66a5424f7ff3cfe8fbd6e9
MarcoDC96 <marco.decicco@live.it>
2023-12-14 16:26:48 +0100

    Final commit, con esempio moto

--- src/org/lessons/flotta/veicoli/Main.java
+++ src/org/lessons/flotta/veicoli/Main.java
@@ -5,6 +5,10 @@ public class Main {
     FlottaVeicoli gestoreFlotta = new FlottaVeicoli();
     Automobile Peugeot208 = new Automobile("AX666CV", 2014, 5);
     gestoreFlotta.aggiungiVeicolo(Peugeot208);
+    System.out.println(gestoreFlotta.contatore());
+    Motocicletta Suzuki420 = new Motocicletta("AZ567BN", 2020, true);
+    gestoreFlotta.aggiungiVeicolo(Suzuki420);
         System.out.println(gestoreFlotta.contatore());
+
     }
 }
